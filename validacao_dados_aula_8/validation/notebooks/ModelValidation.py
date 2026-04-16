7329c172ec36fd696b372177bd87405df0c20576
Luiz Braz <luiz.braz@live.com>
2026-04-16 20:35:30 -0300

    Fix ModelValidation import module

--- validacao_dados_aula_8/validation/notebooks/ModelValidation.py
+++ validacao_dados_aula_8/validation/notebooks/ModelValidation.py
@@ -32,29 +32,21 @@
 
 # COMMAND ----------
 
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
-
-# COMMAND ----------
-
 import os
+spark.range(1).collect()
 notebook_path = '/Workspace/' + os.path.dirname(dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get())
-%cd $notebook_path
 
 # COMMAND ----------
 
-# MAGIC %pip install -r ../../requirements.txt
+# MAGIC %pip install -r $notebook_path/../../requirements.txt
 
 # COMMAND ----------
 
-dbutils.library.restartPython()
-
-# COMMAND ----------
-
-import os
+import os, sys
 notebook_path = '/Workspace/' + os.path.dirname(dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get())
-%cd $notebook_path
-%cd ../
+validation_dir = os.path.dirname(notebook_path)
+if validation_dir not in sys.path:
+    sys.path.insert(0, validation_dir)
 
 # COMMAND ----------
 
