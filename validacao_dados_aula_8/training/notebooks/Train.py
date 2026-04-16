44fa6a94cf5d6c544ffee8043d05a7614016aa77
lfbraz <luiz.braz@live.com>
2026-04-16 23:31:49 +0000

    Remove spark.range

--- validacao_dados_aula_8/training/notebooks/Train.py
+++ validacao_dados_aula_8/training/notebooks/Train.py
@@ -26,7 +26,6 @@
 # COMMAND ----------
 
 import os
-spark.range(1).collect()
 notebook_path = '/Workspace/' + os.path.dirname(dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get())
 
 # COMMAND ----------
